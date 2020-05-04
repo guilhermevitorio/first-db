9cfe228199ca1d0bb7f74aa1e8bb8cad14bd7120
guilhermevitorio <guilhermeevitorio@outlook.com>
2020-05-04 16:25:29 -0300

    Created routes and estruture

--- server.js
+++ server.js
@@ -14,17 +14,8 @@ mongoose.connect('mongodb://localhost:27017/nodeapi',
  useUnifiedTopology: true })
 
  requireDir('./src/models/')
- 
- const Product = mongoose.model('Product')
 
-//first route
-app.get('/', (req, res)=> {
-    Product.create({
-        title: 'react native', 
-        description: 'build native apps',
-        url: 'https://github.com/facebook/react-native'
-    })
-    return res.send('Hello')
-})
+//route
 
+app.use('/api', require('./src/routes'))
 app.listen(3001)
